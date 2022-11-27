3796c2f63d6d540d1f043e134405eaa0b2c5a767
Riverify <CryingRiver@foxmail.com>
2022-11-27 11:29:43 +0800

    使用FactoryBean实例化bean

--- spring_03_bean_instance/src/main/java/com/fubukiss/factory/UserDaoFactoryBean.java
+++ spring_03_bean_instance/src/main/java/com/fubukiss/factory/UserDaoFactoryBean.java
@@ -4,16 +4,20 @@ import com.fubukiss.dao.UserDao;
 import com.fubukiss.dao.impl.UserDaoImpl;
 import org.springframework.beans.factory.FactoryBean;
 
-//FactoryBean创建对象
+// FactoryBean创建对象
 public class UserDaoFactoryBean implements FactoryBean<UserDao> {
-    //代替原始实例工厂中创建对象的方法
+    // 代替原始实例工厂中创建对象的方法
     public UserDao getObject() throws Exception {
         return new UserDaoImpl();
     }
 
-    //UserDao的类型
+    // UserDao的类型
     public Class<?> getObjectType() {
         return UserDao.class;
     }
 
+    // 是否是单例
+    public boolean isSingleton() {
+        return true;
+    }
 }
