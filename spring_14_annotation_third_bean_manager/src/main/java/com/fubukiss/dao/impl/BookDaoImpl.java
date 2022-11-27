f1458f96eeb81252afe05a5518317d6c967d777b
Riverify <CryingRiver@foxmail.com>
2022-11-27 19:18:42 +0800

    第三方bean应用

---
+++ spring_14_annotation_third_bean_manager/src/main/java/com/fubukiss/dao/impl/BookDaoImpl.java
@@ -0,0 +1,12 @@
+package com.fubukiss.dao.impl;
+
+import com.fubukiss.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+
+    public void save() {
+        System.out.println("book dao save ...");
+    }
+}
